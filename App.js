17cf708dd41abdef70eb90e6165adaa0428fe213
Khun Kyaw Kham Hti <khunkyawkhamhti@Khuns-Air.local>
2023-02-14 13:27:39 +0630

    second commit
    some minor modification

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { useEffect, useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from './src/views/screens/HomeScreen';
-import LoginScreen from './src/views/screens/LoginScreen';
-import RegisterationScreen from './src/views/screens/RegisterationScreen';
-import Loader from './src/views/conponents/Loader';
+import HomeScreen from "./src/views/screens/HomeScreen";
+import LoginScreen from "./src/views/screens/LoginScreen";
+import RegisterationScreen from "./src/views/screens/RegisterationScreen";
+import Loader from "./src/views/conponents/Loader";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const [initialRouteName, setInitialRouteName ] = useState('');
+  const [initialRouteName, setInitialRouteName] = useState(" ");
 
   useEffect(() => {
     setTimeout(() => {
@@ -24,37 +24,40 @@ const App = () => {
       let userData = await AsyncStorage.getItem(userData);
       if (userData) {
         userData = JSON.parse(userData);
-        if(userData.loggedIn) {
-          setInitialRouteName('HomeScreen');
-        }else {
-          setInitialRouteName('LoginScreen');
+        if (userData.loggedIn) {
+          setInitialRouteName("HomeScreen");
+        } else {
+          setInitialRouteName("LoginScreen");
         }
-      }else {
-        setInitialRouteName('RegisterationScreen');
+      } else {
+        setInitialRouteName("RegisterationScreen");
       }
     } catch (error) {
-      setInitialRouteName('RegisterationScreen');
+      setInitialRouteName("RegisterationScreen");
     }
   };
 
   return (
     <NavigationContainer>
-     {!initialRouteName ? (
-     <Loader visible={true} />
-     ):(
-     <> 
-        <Stack.Navigator 
-        initialRouteName={initialRouteName}
-        screenOptions={{headerShown: false}} >
-          <Stack.Screen name='RegisterationScreen' component={RegisterationScreen} />
-          <Stack.Screen name='LoginScreen' component={LoginScreen} />
-          <Stack.Screen name='HomeScreen' component={HomeScreen} />
-        </Stack.Navigator> 
-     </>
-  )}
+      {!initialRouteName ? (
+        <Loader visible={true} />
+      ) : (
+        <>
+          <Stack.Navigator
+            initialRouteName={initialRouteName}
+            screenOptions={{ headerShown: false }}
+          >
+            <Stack.Screen
+              name="RegisterationScreen"
+              component={RegisterationScreen}
+            />
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          </Stack.Navigator>
+        </>
+      )}
     </NavigationContainer>
   );
 };
 
 export default App;
-
